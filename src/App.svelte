f77b91bd8d466b39d768ae2cfe43907fec1fe364
George <georgesimbe@icloud.com>
2021-07-14 14:00:26 +0930

    - Created Header - Created Footer - Removed Padding on global css - Added Logo in img file

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
 <script>
-
+import Header from './components/Header.svelte'
+import Footer from './components/Footer.svelte'
 </script>
 
+<Header/>
 <main>
-	<p>Poll Site</p>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis praesentium, sapiente unde voluptas aperiam eaque vero quos eum animi dicta facilis laudantium iste ea provident harum doloremque, quae quam optio!</p>
 </main>
-
+<Footer/>
 <style>
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
 
 </style> 
---
+++ src/components/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+
+import App from "../App.svelte";
+
+</script>
+
+
+<header>
+<h1> 
+    <img src="/img/pollLogo.png" alt="Logo for polls">
+    
+</h1>
+<h1>Simple Polls</h1>
+
+</header>
+
+<style>
+	Header{
+		background: #f7f7f7;
+		padding: 5px;
+	}
+	h1{
+		margin: 0;
+		text-align: center;
+	}
+    img{
+        max-width: 160px;
+    }
+</style>
